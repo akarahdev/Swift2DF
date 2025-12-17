ad12183042584d1fe658822a35ffa0a7baac1c85
akarahdev <endisticbutitsforgit@gmail.com>
2025-12-17 17:38:09 -0500

    feat: refactor project into Swift2DF and Swift2DFPlayground

--- Package.swift
+++ Package.swift
@@ -4,31 +4,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift2df",
+    name: "Swift2DF",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift2df",
-            targets: ["swift2df"]
+            name: "Swift2DF",
+            targets: ["Swift2DF"]
         ),
+        .executable(
+            name: "Swift2DFPlayground", 
+            targets: ["Swift2DFPlayground"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/1024jp/GzipSwift", from: Version(6, 0, 0)),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: Version(2, 16, 1))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift2df",
+            name: "Swift2DF",
             dependencies: [
                 .product(name: "Gzip", package: "GzipSwift"),
                 .product(name: "WebSocketKit", package: "websocket-kit")
             ]
         ),
-        .testTarget(
-            name: "swift2dfTests",
-            dependencies: ["swift2df"]
-        ),
+        .executableTarget(
+            name: "Swift2DFPlayground",
+            dependencies: [
+                .product(name: "Gzip", package: "GzipSwift"),
+                .product(name: "WebSocketKit", package: "websocket-kit"),
+                .byName(name: "Swift2DF")
+            ]
+        )
     ]
 )
