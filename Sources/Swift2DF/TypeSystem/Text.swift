cd9f05992d21e16c6be1ea3898d29adcdec31153
akarahdev <endisticbutitsforgit@gmail.com>
2025-12-17 23:57:54 -0500

    feat: add  for minimessage parsing

--- Sources/Swift2DF/TypeSystem/Text.swift
+++ Sources/Swift2DF/TypeSystem/Text.swift
@@ -8,7 +8,7 @@ public func text(_ value: AnyValue) -> Text {
     let variable = VariableVarItem.generateRandomly()
     GENERATED_CODE.append(
         SelectionBlock.setVar(
-            action: "StyledText", 
+            action: "StyledText",
             args: [
                 0: variable,
                 1: value.varItem
@@ -16,6 +16,22 @@ public func text(_ value: AnyValue) -> Text {
         )
         .tagged(slot: 25, tag: "Inherit Styles", option: "True")
         .tagged(slot: 26, tag: "Text Value Merging", option: "No spaces")
+    )
+    return Text(varItem: variable)
+}
+
+public func mm(_ value: String) -> Text {
+    let variable = VariableVarItem.generateRandomly()
+    GENERATED_CODE.append(
+        SelectionBlock.setVar(
+            action: "ParseMiniMessage",
+            args: [
+                0: variable,
+                1: value.varItem
+            ]
+        )
+        .tagged(slot: 25, tag: "Parse Legacy Color Codes", option: "False")
+        .tagged(slot: 26, tag: "Allowed Tags", option: "Full")
     )
     return Text(varItem: variable)
 }
